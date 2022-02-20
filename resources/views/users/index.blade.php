3f8ce2a6337c1bf927c4676b101c2fe4402bd929
andikajayaw <andikajayawiguna@gmail.com>
2022-02-20 23:32:49 +0800

    finishing CRUD for Users module, adding Paginator to App/Service/Provider

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -6,16 +6,62 @@ User List
 
 @section('content')
 
-    Daftar User disini
+    @if(session('status'))
+        <div class="alert alert-success">
+            {{ session('status') }}
+        </div>
+    @endif
 
+    <form action="{{route('users.index')}}">
+        <div class="row">
+            <div class="col-md-6">
+                <input
+                value="{{Request::get('keyword')}}" 
+                name="keyword" 
+                class="form-control col-md-10" 
+                type="text"
+                placeholder="Filter berdasarkan email"/>
+            </div>
+            <div class="col-md-6">
+                <input {{Request::get('status') == 'ACTIVE' ? 'checked' : ''}} 
+                value="ACTIVE" 
+                name="status" 
+                type="radio" 
+                class="form-control" 
+                id="active">
+                <label for="active">Active</label>
+
+                <input {{Request::get('status') == 'INACTIVE' ? 'checked' : ''}} 
+                value="INACTIVE" 
+                name="status" 
+                type="radio" 
+                class="form-control" 
+                id="inactive">
+                <label for="inactive">Inactive</label>
+
+                <input 
+                type="submit" 
+                value="Filter" 
+                class="btn btn-primary">
+            </div>
+        </div>
+    </form>
+
+    <div class="row">
+        <div class="col-md-12 text-right">
+            <a href="{{ route('users.create') }}" class="btn btn-primary">Buat User</a>
+        </div>
+    </div>
+    <br>
     <table class="table table-bordered">
         <thead>
             <tr>
-            <th><b>Name</b></th>
-            <th><b>Username</b></th>
-            <th><b>Email</b></th>
-            <th><b>Avatar</b></th>
-            <th><b>Action</b></th>
+                <th><b>Name</b></th>
+                <th><b>Username</b></th>
+                <th><b>Email</b></th>
+                <th><b>Avatar</b></th>
+                <th><b>Status</b></th>
+                <th><b>Action</b></th>
             </tr>
         </thead>
         <tbody>
@@ -30,13 +76,34 @@ User List
                     @else 
                     N/A
                     @endif
-
                 </td>
                 <td>
+                    @if($user->status == "ACTIVE")
+                        <span class="badge badge-success">
+                            {{ $user->status }}
+                        </span>
+                    @else
+                        <span class="badge badge-danger">
+                            {{ $user->status }}
+                        </span>
+                    @endif
+                </td>
+                <td>
+                    <a href="{{ route('users.show', [$user->id]) }}" class="btn btn-primary btn-sm">Detail</a>
                     <a class="btn btn-info text-white btn-sm" href="{{route('users.edit', [$user->id])}}">Edit</a>
+                    <form action="{{ route('users.destroy', [$user->id]) }}" method="post" onsubmit="return confirm('Hapus user ini?')" class="d-inline">
+                        @csrf
+                        <input type="hidden" name="_method" value="DELETE">
+                        <input type="submit" value="Delete" class="btn btn-danger btn-sm">
+                    </form>
                 </td>
             </tr>
             @endforeach
         </tbody>
+        <tfoot>
+            <tr>
+                <td colspan="6">{{ $users->appends(Request::all())->links() }}</td>
+            </tr>
+        </tfoot>
     </table>
 @endsection
